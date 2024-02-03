b89e04cded8d771aa5728050a512a855eb697fd4
yoyo0527 <C110156132@nkust.edu.tw>
2024-02-03 21:25:45 +0800

    09:24

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './css/styles.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import { HomePage } from './Homepage';
+import { AboutPage } from './Aboutpage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/nailbook" element ={<HomePage/>} />
+          <Route path="/nailbook/about" element ={<AboutPage/>} />
         </Routes>
       </Router>
     </div>
--- src/Homepage.js
+++ src/Homepage.js
@@ -1,72 +1,60 @@
 import './css/styles.css';
 import { useNavigate } from 'react-router-dom';
+import intro from './assets/img/intro.png';
+import './App.css';
 
 export function HomePage() {
-  const navigate = useNavigate();
-  const handleHome = () => {
-    navigate('/nailbook');
-  };
+    const navigate = useNavigate();
 
-  return (
-    <div className="App">
-        <header>
-            <h1 className="site-heading text-center text-faded d-none d-lg-block">
-                <span className="site-heading-upper text-primary mb-3">A Free Bootstrap Business Theme</span>
-                <span className="site-heading-lower">Nail Disease</span>
-            </h1>
-        </header>
-        {/* <!-- Navigation--> */}
-        <nav className="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
-            <div className="container">
-                <a className="navbar-brand text-uppercase fw-bold d-lg-none" href="index.html">Start Bootstrap</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mx-auto">
-                        <li className="nav-item px-lg-4"><a className="nav-link text-uppercase" onClick={{handleHome}}>Home</a></li>
-                        <li className="nav-item px-lg-4"><a className="nav-link text-uppercase" onClick={{}}>About</a></li>
-                        <li className="nav-item px-lg-4"><a className="nav-link text-uppercase" onClick={{}}>Products</a></li>
-                        <li className="nav-item px-lg-4"><a className="nav-link text-uppercase" onClick={{}}>Store</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <section className="page-section clearfix">
-            <div className="container">
-                <div className="intro">
-                    <img className="intro-img img-fluid mb-3 mb-lg-0 rounded" src="assets/img/intro.jpg" alt="..." />
-                    <div className="intro-text left-0 text-center bg-faded p-5 rounded">
-                        <h2 className="section-heading mb-4">
-                            <span className="section-heading-upper">Fresh Coffee</span>
-                            <span className="section-heading-lower">Worth Drinking</span>
-                        </h2>
-                        <p className="mb-3">Every cup of our quality artisan coffee starts with locally sourced, hand picked ingredients. Once you try it, our coffee will be a blissful addition to your everyday morning routine - we guarantee it!</p>
-                        <div className="intro-button mx-auto"><a className="btn btn-primary btn-xl" href="#!">Visit Us Today!</a></div>
+    function handleHome() {
+        navigate('/nailbook');
+    }
+
+    function handleAbout() {
+        navigate('/nailbook/about');
+    }
+
+    return (
+        <div>
+            <header>
+                <h1 className="site-heading text-center text-faded d-none d-lg-block">
+                    <span className="site-heading-upper text-primary mb-3">真甲難辨</span>
+                    <span className="site-heading-lower">Nail Disease</span>
+                </h1>
+            </header>
+            {/* <!-- Navigation--> */}
+            <nav className="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
+                <div className="container">
+                    <a className="navbar-brand text-uppercase fw-bold d-lg-none" href="index.html">Nail Identification</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mx-auto">
+                            <li className="nav-item px-lg-4"><a className="nav-link text-uppercase" onClick={handleHome}>首頁</a></li>
+                            <li className="nav-item px-lg-4"><a className="nav-link text-uppercase" onClick={handleAbout}>組員介紹</a></li>
+                            <li className="nav-item px-lg-4"><a className="nav-link text-uppercase" onClick={handleAbout}>目的</a></li>
+                            <li className="nav-item px-lg-4"><a className="nav-link text-uppercase" onClick={handleAbout}>運用技術</a></li>
+                            <li className="nav-item px-lg-4"><a className="nav-link text-uppercase" onClick={handleAbout}>測驗</a></li>
+                        </ul>
                     </div>
                 </div>
-            </div>
-        </section>
-        <section className="page-section cta">
-            <div className="container">
-                <div className="row">
-                    <div className="col-xl-9 mx-auto">
-                        <div className="cta-inner bg-faded text-center rounded">
+            </nav>
+            <section className="page-section clearfix">
+                <div className="container">
+                    <div className="intro">
+                        <img className="intro-img img-fluid mb-3 mb-lg-0 rounded" src={intro} alt="..." />
+                        <div className="intro-text left-0 text-center bg-faded p-5 rounded">
                             <h2 className="section-heading mb-4">
-                                <span className="section-heading-upper">Our Promise</span>
-                                <span className="section-heading-lower">To You</span>
+                                <span className="section-heading-lower"><b>背景</b></span>
                             </h2>
-                            <p className="mb-0">When you walk into our shop to start your day, we are dedicated to providing you with friendly service, a welcoming atmosphere, and above all else, excellent products made with the highest quality ingredients. If you are not satisfied, please let us know and we will do whatever we can to make things right!</p>
+                            <p className="mb-3">手腳指甲是人體健康的一個指標，指甲的外觀可以反映出身體健康狀況的一些跡象。然而，許多人可能未能及時察覺或了解指甲變化的​重要性，因此這項應用的開發是為了幫助用戶更容易地識別並了解可能與指甲健康相關的​問題。​</p>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
-        <footer className="footer text-faded text-center py-5">
-            <div className="container"><p className="m-0 small">Copyright &copy; Your Website 2023</p></div>
-        </footer>
-        {/* <!-- Bootstrap core JS--> */}
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-        {/* <!-- Core theme JS--> */}
-        <script src="js/scripts.js"></script>    </div>
-  );
+            </section>
+            <footer className="footer text-faded text-center py-5">
+                <div className="container"><p className="m-0 small">Copyright &copy; Your Website 2024</p></div>
+            </footer>
+        </div>
+    );
 }
 
