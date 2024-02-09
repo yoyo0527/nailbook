7b26fb0c52ced6d764a3ce43a0ec36a11731e46c
yoyo0527 <C110156132@nkust.edu.tw>
2024-02-09 16:34:13 +0800

    0434

--- src/Diseasepage.js
+++ src/Diseasepage.js
@@ -5,6 +5,11 @@ import graynail2 from './assets/img/graynail2.gif';
 import clubbingnail from './assets/img/clubbing.jpg';
 import clubbingnail2 from './assets/img/clubbing2.jpg';
 import clubbingnail3 from './assets/img/clubbing3.webp';
+import beaus from './assets/img/beaus.jpg';
+import beaus2 from './assets/img/beaus2.jpg';
+import blackline from './assets/img/blackline.jpg';
+import blackline2 from './assets/img/blackline2.png';
+import blackline3 from './assets/img/blackline3.webp';
 
 import './App.css';
 
@@ -107,7 +112,7 @@ export function DiseasePage() {
                                 </h2>
                             </div>
                         </div>
-                        <div className="product-item-description d-flex me-auto">
+                        <div className="product-item-description mx-auto d-flex rounded mb-3 mb-lg-0">
                             <div className="bg-faded p-5 rounded">
                                 <p className="contentfont mb-0">主要方式是藥物治療，分為外用跟口服，但通常會同時配合手術「挫甲」（拔除壞死及感染的指甲）、雷射等輔助治療。​</p>
                                 <p className="contentfont mb-0"><b>外用藥</b></p>
@@ -132,7 +137,7 @@ export function DiseasePage() {
                                 </h2>
                             </div>
                         </div>
-                        <div className="product-item-description d-flex ms-auto">
+                        <div className="product-item-description mx-auto d-flex rounded mb-3 mb-lg-0">
                             <div className="bg-faded p-5 rounded">
                                 <ul>
                                     <li><p className="contentfont mb-0">指甲出現變形、混濁，儘快去看皮膚科醫師​</p></li>
@@ -185,7 +190,7 @@ export function DiseasePage() {
                         <div className="product-item-description d-flex ms-auto">
                             <div className="bg-faded p-5 rounded">
                                     <p className="contentfont mb-0">目前醫學上還無法完全確認杵狀指的發生原因，目前主要是認為當心肺相關疾病導致身體缺氧時，會刺激患者的血液中都會有血管內皮生長因子（vascular endothelial growth factor, VEGF）增加。VEGF 是一種血小板衍生因子，會誘使指甲處發生血管增生、水腫、纖維母細胞或成骨細胞增生並進一步造成杵狀指。​</p>
-                                    <p className="contentfont mb-0">此外，會造成血小板慢性增加的發炎性腸道疾病也會因為生長因子增加造成杵狀指發生。​ ​</p>
+                                    <p className="contentfont mb-0">此外，會造成血小板慢性增加的發炎性腸道疾病也會因為生長因子增加造成杵狀指發生。​​</p>
                             </div>
                         </div>
                     </div>
@@ -217,17 +222,197 @@ export function DiseasePage() {
                                 </h2>
                             </div>
                         </div>
-                        <div className="product-item-description d-flex ms-auto">
+                        <div className="product-item-description mx-auto d-flex rounded mb-3 mb-lg-0">
                             <div className="bg-faded p-5 rounded">
                                     <p className="contentfont mb-0">杵狀指多數是由於外部因素以及內部疾病所引起的，所以在進行治療的時候應對原發病進行治療。當原發病得到治療控制後，杵狀指的症狀也會得到緩解​</p>
-                                    <p className="contentfont mb-0"><b>杵狀指看哪一科？​</b> ​</p>
+                                    <p className="contentfont mb-0"><b>杵狀指看哪一科？​</b>​</p>
                                     <p className="contentfont mb-0">若懷疑發生杵狀指而不知道要掛哪一科，可以先至家醫科就醫，由家醫科先做心肺評估。​</p>
                                     <p className="contentfont mb-0">若發現自己有杵狀指且容易喘、會聽到喘鳴聲、吸氣吸不飽等狀況，則可直接尋求胸腔科協助。​</p>
                                     <p className="contentfont mb-0">如果肺功能評估沒問題，病人本身也沒有抽菸，就可能是心衰竭或是心臟結構異常，需至心臟科就醫。​</p>
                                     <p className="contentfont mb-0">如果有長期腹瀉腹痛等問題，也必須尋求腸胃內科醫師協助排除發炎性腸道疾病。​</p>
-                                    <p className="contentfont mb-0"><b>杵狀指是肺癌前兆​</b> ​</p>
+                                    <p className="contentfont mb-0"><b>杵狀指是肺癌前兆​</b>​</p>
                                     <p className="contentfont mb-0">發現自己有杵狀指，先不必驚慌，病人可能本身原本就有肺部功能問題而導致杵狀指發生。有杵狀指也可能是老菸槍、嚴重慢性肺阻塞的病人或者罹患其他心臟相關疾病、腸胃道功能有狀況等。並不是肺癌才會發現杵狀指。​</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
 
+            {/* 博氏線 */}
+            <section className="page-section">
+                <div className="container">
+                    <div className="product-item">
+                        <div className="product-item-title d-flex">
+                            <div className="bg-faded2 p-5 d-flex ms-auto rounded">
+                                <h2 className="section-heading mb-0">
+                                    <span className="section-heading-upper">指甲疾病</span>
+                                    <span className="section-heading-lower">博氏線</span>
+                                </h2>
+                            </div>
+                        </div>
+                        <img className="img-detect mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src={beaus} alt="..." />
+                        <div className="product-item-description d-flex me-auto">
+                            <div className="bg-faded p-5 rounded">
+                                <p className="contentfont mb-0">博氏線會有凹痕或凹陷，指甲表面可能變得粗糙或不光滑。</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className="page-section">
+                <div className="container">
+                    <div className="product-item">
+                        <div className="product-item-title d-flex">
+                            <div className="bg-faded2 p-5 d-flex me-auto rounded">
+                                <h2 className="section-heading mb-0">
+                                    <span className="section-heading-lower">博氏線的原因​</span>
+                                </h2>
+                            </div>
+                        </div>
+                        <img className="img-detect mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src={beaus2} alt="..." />
+                        <div className="product-item-description d-flex ms-auto">
+                            <div className="bg-faded p-5 rounded">
+                                <ul>
+                                    <li><p className="contentfont mb-0">近期患有重病、承受重大壓力或罹患特定疾病，造成身體營養及能量無法傳送至末端指甲。​</p></li>
+                                    <li><p className="contentfont mb-0">指甲基質受到壓迫，導致指甲生長冒出凹痕。​</p></li>
+                                    <li><p className="contentfont mb-0">患有嚴重皮膚問題，損害指甲基質。</p></li>
+                                    <li><p className="contentfont mb-0">缺乏特定營養素，影響指甲生成。</p></li>
+                                </ul>
+                                    
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className="page-section">
+                <div className="container">
+                    <div className="product-item">
+                        <div className="product-item-title d-flex">
+                            <div className="bg-faded2 p-5 d-flex ms-auto rounded">
+                                <h2 className="section-heading mb-0">
+                                    <span className="section-heading-lower">對照上述4成因，以下有更詳細的解釋：</span>
+                                </h2>
+                            </div>
+                        </div>
+                        <img className="img-detect mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src={beaus} alt="..." />
+                        <div className="product-item-description d-flex me-auto">
+                            <div className="bg-faded p-5 rounded">
+                            <ul>
+                                    <li><p className="contentfont mb-0"><b>身體疾病​</b></p></li>
+                                    <ul>
+                                        <li><p className="contentfont mb-0">肺炎​</p></li>
+                                        <li><p className="contentfont mb-0">鏈球菌感染​</p></li>
+                                        <li><p className="contentfont mb-0">腮腺炎</p></li>
+                                        <li><p className="contentfont mb-0">麻疹</p></li>
+                                        <li><p className="contentfont mb-0">長時間高燒​</p></li>
+                                        <li><p className="contentfont mb-0">心臟病​</p></li>
+                                        <li><p className="contentfont mb-0">新冠肺炎​</p></li>
+                                        <li><p className="contentfont mb-0">糖尿病</p></li>
+                                        <li><p className="contentfont mb-0">甲狀腺低下​</p></li>
+                                        <li><p className="contentfont mb-0">週邊血管疾病​</p></li>
+                                        <li><p className="contentfont mb-0">雷諾氏現象​</p></li>
+                                    </ul>
+                                    <li><p className="contentfont mb-0"><b>指甲基質受外在因素影響​</b></p></li>
+                                    <ul>
+                                        <li><p className="contentfont mb-0">指甲基質受重物壓迫​</p></li>
+                                        <li><p className="contentfont mb-0">暴露於嚴寒環境​</p></li>
+                                        <li><p className="contentfont mb-0">做指甲感染或不當修剪指甲​</p></li>
+                                    </ul>
+                                    <li><p className="contentfont mb-0"><b>皮膚疾病</b></p></li>
+                                    <ul>
+                                        <li><p className="contentfont mb-0">濕疹</p></li>
+                                        <li><p className="contentfont mb-0">乾癬</p></li>
+                                        <li><p className="contentfont mb-0">缺乏特定營養素​</p></li>
+                                        <li><p className="contentfont mb-0">缺乏鋅</p></li>
+                                        <li><p className="contentfont mb-0">缺乏蛋白質​</p></li>
+                                    </ul>
+                                    <li><p className="contentfont mb-0"><b>值得注意的是，嚴重的精神壓力和心理疾病，也可能影響指甲健康，造成博氏線的出現。</b></p></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            {/* 縱向黑甲症 */}
+            <section className="page-section">
+                <div className="container">
+                    <div className="product-item">
+                        <div className="product-item-title d-flex">
+                            <div className="bg-faded2 p-5 d-flex me-auto rounded">
+                                <h2 className="section-heading mb-0">
+                                    <span className="section-heading-upper">指甲疾病</span>
+                                    <span className="section-heading-lower">縱向黑甲症</span>
+                                </h2>
+                            </div>
+                        </div>
+                        <img className="img-detect mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src={blackline} alt="..." />
+                        <div className="product-item-description d-flex ms-auto">
+                            <div className="bg-faded p-5 rounded">
+                                    <p className="contentfont mb-0">指甲上的黑線超過95%為淡灰色、深黑色或淡棕色。指甲有黑線其實並不稀奇，不妨想像成甲床上長了痣，只是在指甲上以黑線的方式呈現。雖然任何年齡層都有可能在手指或腳趾出現，不過除了黑色素細胞活化外，<b>以下情況也可能會讓你的指甲長出黑線：</b></p>
+                                <ul>
+                                    <li><p className="contentfont mb-0"><b>黴菌感染：</b>有些黴菌感染會產生黑色素堆積在指甲上，造成指甲出現線狀或塊狀黑色素沉積。​</p></li>
+                                    <li><p className="contentfont mb-0"><b>疾病病徵：</b>甲狀腺亢進、營養不良、愛滋病、乾癬等都會讓指甲出現黑線。​</p></li>
+                                    <li><p className="contentfont mb-0"><b>血塊淤積：</b>甲床下出血可能導致血液積聚於甲床變得暗沉，導致指甲出現黑線。​</p></li>
+                                    <li><p className="contentfont mb-0"><b>藥物影響：</b>化a療藥物、治療青春痘的四環黴素。​</p></li>
+                                    <li><p className="contentfont mb-0"><b>外傷：</b>摳抓、啃咬指甲造成色素沉澱。​</p></li>
+                                    <li><p className="contentfont mb-0"><b>種族基因：</b>相較於亞洲人，黑人的指甲較容易出現黑線，據統計，約有77%的黑人到20歲前會出現縱向黑甲症。​</p></li>
+                                </ul>
+                                    
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className="page-section">
+                <div className="container">
+                    <div className="product-item">
+                        <div className="product-item-title d-flex">
+                            <div className="bg-faded2 p-5 d-flex ms-auto rounded">
+                                <h2 className="section-heading mb-0">
+                                    <span className="section-heading-lower">縱向黑甲症​</span>
+                                </h2>
+                            </div>
+                        </div>
+                        <img className="img-detect mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src={blackline2} alt="..." />
+                        <div className="product-item-description d-flex me-auto">
+                            <div className="bg-faded p-5 rounded">
+                                <p className="contentfont mb-0">雖然指甲長黑線大多屬於良性變化，不過當你發現指甲的黑線<b>符合下列情況時，則需留意是否為罹患黑色素瘤的高風險患者</b>（Melanoma，或稱黑色素細胞癌），建議盡快就醫由醫師進行專業診斷：​</p>
+                                <p className="contentfont mb-0"><b>1.</b>黑線開始往旁邊擴散​</p>
+                                <p className="contentfont mb-0"><b>2.</b>顏色深淺不一​</p>
+                                <p className="contentfont mb-0"><b>3.</b>寬窄粗細不一樣​</p>
+                                <p className="contentfont mb-0"><b>4.</b>黑線不規則變黑​</p>
+                                <p className="contentfont mb-0"><b>5.</b>指甲全黑​</p>
+                                <p className="contentfont mb-0"><b>6.</b>指甲附近出現跳躍性斑塊​</p>
+                                <p className="contentfont mb-0"><b>7.</b>指甲附近皮膚變黑​</p>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className="page-section">
+                <div className="container">
+                    <div className="product-item">
+                        <div className="product-item-title d-flex">
+                            <div className="bg-faded2 p-5 d-flex me-auto rounded">
+                                <h2 className="section-heading mb-0">
+                                    <span className="section-heading-lower">指甲黑線是否會自行消失​</span>
+                                </h2>
+                            </div>
+                        </div>
+                        <img className="img-detect mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src={blackline3} alt="..." />
+                        <div className="product-item-description d-flex ms-auto">
+                            <div className="bg-faded p-5 rounded">
+                                <ul>
+                                    <li><p className="contentfont mb-0">因<b>多數指甲黑線屬良性</b>，若只是單純黑色素細胞活化，或因口服藥物（四環黴素、化療藥物）、外傷等原因，只要停藥或是待外傷痊癒，過段時間就會自行消失。​</p></li>
+                                    <li><p className="contentfont mb-0">不過若是<b>因為疾病而產生的黑線</b>，例如甲狀腺亢進、營養不良、乾癬、粘液囊腫、全身性紅斑性狼瘡等，則需要等疾病治療痊癒，指甲的黑線才會消失。​</p></li>
+                                </ul>
+                                    
                             </div>
                         </div>
                     </div>
